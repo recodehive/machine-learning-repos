ba089a6231b5fbe59c9837da9f558bd71cd5af77
unknown <10verma2002aman@gmail.com>
2024-07-15 11:39:11 +0530

    added

--- Detecting-IoT-Sensor-fault-using-ML/sensor/constant/database.py
+++
@@ -1,3 +0,0 @@
-DATABASE_NAME = "Sensor_databse"
-COLLECTION_NAME = "sensor_failure"
-#MONGODB_URL_KEY = "mongodb+srv://vaasu:pcvaasud9sp@cluster1.r5ecagf.mongodb.net/?retryWrites=true&w=majority"
--- Detecting-IoT-Sensor-fault-using-ML/sensor/constant/s3_bucket.py
+++
@@ -1,2 +0,0 @@
-TRAINING_BUCKET_NAME = "truckai-sensor"
-PREDICTION_BUCKET_NAME = "sensor-datasource"
