3f40223fecf7912c3f61a862d48e8c722e848a97
Sanjay Viswanathan <sanjay.kanakkot@gmail.com>
2024-10-08 17:37:12 +1100

    Delete Predict-Ethereum directory

--- Predict-Ethereum/mainapp/app.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainapp'
--- Predict-Ethereum/mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views
-
-app_name = 'mainapp'
-
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('predict_ethereum_price/',views.predict,name='predict'),
-]
